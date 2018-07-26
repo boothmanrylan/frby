9223e46521c8ecca7891b6ad5ec6965ffea4a2a2
Rylan Boothman <boothmanrylan@gmail.com>
2018-07-26 12:24:56 -0400

    Check if groups and datasets have been created before creating them

--- mkdata/mkdata.py
+++ mkdata/mkdata.py
@@ -2,7 +2,7 @@ import os
 import h5py
 import glob
 import numpy as np
-# from mpi4py import MPI
+from mpi4py import MPI
 import astropy.units as u
 from frb import FRB
 from pulsar import Pulsar
@@ -25,7 +25,7 @@ frb_args = {'t_ref': 0 * u.ms,
 psr_args = {'t_ref': 0 * u.ms,
             'f_ref': 600 * u.MHz,
             'dm': (2, 25) * (u.pc / u.cm**3),
-            'fluence': (0.01, 10) * (u.Jy * u.ms),
+            'fluence': (0.01, 5) * (u.Jy * u.ms),
             'freq': (800, 400) * u.MHz,
             'rate': (400 / 1024) * u.MHz,
             'scat_factor': (-5, -4),
@@ -36,7 +36,12 @@ psr_args = {'t_ref': 0 * u.ms,
             'period': (2, 5e2) * u.ms}
 
 def save_to_hdf5(event, attributes, hdf5_file, path):
-    dset = hdf5_file.create_dataset(path, data=event)
+    # Save the event unless the path already exists at which point return
+    # without saving or overwritten the old data.
+    try:
+        dset = hdf5_file.create_dataset(path, data=event)
+    except RuntimeError:
+        return
     for key, value in attributes.items():
         try:
             dset.attrs[key] = value
@@ -52,7 +57,6 @@ def inject_and_save(rfi_event, hdf5_file, sim_number, path):
     psr_args['rate'] = rfi_event.rfi.shape[1]/rfi_event.attributes['duration']
     psr = Pulsar(**psr_args)
     save_to_hdf5(psr.pulsar, psr.attributes, file, psr_path)
-    return
 
     frb_path = '/frb/{}/{}'.format(path, sim_number)
     frb_args['background'] = rfi_event.rfi
@@ -63,7 +67,10 @@ def inject_and_save(rfi_event, hdf5_file, sim_number, path):
 def make_data(rfi_class, files, kwargs, key, rank, hdf5_file):
     n_mods = len(modifications)
 
-    rfi = rfi_class(files, **kwargs)
+    if key == 'telescope':
+        rfi = rfi_class(files, **kwargs)
+    else:
+        rfi = rfi_class(**kwargs)
     path = '{}/unmodified'.format(key)
     inject_and_save(rfi, hdf5_file, rank * n_mods, path)
 
@@ -73,8 +80,9 @@ def make_data(rfi_class, files, kwargs, key, rank, hdf5_file):
             rfi.apply_function(m['func'], name=m['name'], input=m['input'],
                                freq_range=m['freq_range'],
                                time_range=m['time_range'], **m['params'])
-        path = '{}/modified'.format(RFI['key'])
+        path = '{}/modified'.format(key)
         inject_and_save(rfi, hdf5_file, (rank * n_mods) + idx, path)
+        print('Done {}/{} of {}'.format(idx + 1, n_mods, key))
 
         # Unapply the modification to be able to apply another to the same base
         rfi.reset()
@@ -82,28 +90,42 @@ def make_data(rfi_class, files, kwargs, key, rank, hdf5_file):
     duration = rfi.attributes['duration']
     return shape, duration
 
-# comm = MPI.COMM_WORLD
-# rank = comm.Get_rank()
-# size = comm.Get_size()
-rank = 0
-size = 1
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
 file_name = '/home/rylan/dunlap/data/training_data.hdf5'
-file = h5py.File(file_name, 'w')
+file = h5py.File(file_name, 'r+')
 
-rfi_grp = file.create_group('/rfi')
-psr_grp = file.create_group('/psr')
-frb_grp = file.create_group('/frb')
+# Create the HDF5 groups if they do not already exist
+try:
+    rfi_grp = file.create_group('/rfi')
+except ValueError:
+    rfi_grp = file['/rfi']
+try:
+    psr_grp = file.create_group('/psr')
+except ValueError:
+    psr_grp = file['/psr']
+try:
+    frb_grp = file.create_group('/frb')
+except:
+    frb_grp = file['/frb']
 for grp in [rfi_grp, psr_grp, frb_grp]:
     for sub_grp in ['telescope', 'solid', 'poisson', 'uniform', 'normal']:
-        sub_sub_grp = grp.create_group(sub_grp)
-        sub_sub_grp.create_group('unmodified')
-        sub_sub_grp.create_group('modified')
+        try:
+            sub_grp = grp.create_group(sub_grp)
+        except ValueError:
+            sub_grp = grp[sub_grp]
+        try:
+            sub_grp.create_group('unmodified')
+            sub_grp.create_group('modified')
+        except ValueError:
+            pass
 
 aro_files = np.sort(glob.glob('/home/rylan/dunlap/data/vdif/*.vdif'))
 group = 20
 
-while aro_files.shape[0] - group < size:
+while aro_files.shape[0] - group <= size:
     aro_files = np.hstack([aro_files] * 2)
 
 rank_files = sf.open(aro_files[rank: rank + group])
@@ -112,7 +134,7 @@ rank_files = sf.open(aro_files[rank: rank + group])
 # of data made through this method as the shape and duration of data made in
 # the other methods
 shape, duration = make_data(TelescopeRFI, rank_files, {'rate_out': 1e-3*u.MHz},
-                            rank, file)
+                           'telescope', rank, file)
 
 normal_rfi = {'class': NormalRFI,
               'key': 'normal',
