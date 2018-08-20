6c3ab1317ec790ec97cb1dee9b592b2f2ddd3de8
Rylan Boothman <boothmanrylan@gmail.com>
2018-08-20 07:46:17 -0400

    Add class to read data from .npy files, stop saving height and width of data in tfrecords

--- estimator/dataset.py
+++ estimator/dataset.py
@@ -1,26 +1,22 @@
 import tensorflow as tf
+import numpy as np
+import glob
 import definitions
+#definitions.height and definitions.width hold the height and width of the data
 
 class Dataset(object):
     def __init__(self, pattern, training=True):
         self.pattern = pattern
         self.training = training
 
-    def get_tfrecords(self):
-        records = tf.data.Dataset.list_files(self.pattern)
-        return records
-
     def parser(self, example):
         features = tf.parse_single_example(
             example,
             features={
                 'data': tf.FixedLenFeature([], tf.string),
-                'label': tf.FixedLenFeature([], tf.int64),
-                'height': tf.FixedLenFeature([], tf.int64),
-                'width': tf.FixedLenFeature([], tf.int64),
+                'label': tf.FixedLenFeature([], tf.int64)
             })
-        height = tf.cast(features['height'], tf.int64)
-        width = tf.cast(features['width'], tf.int64)
+
         # Make the data 'channels_first' with only one channel
         shape = tf.stack([1, definitions.height, definitions.width])
 
@@ -35,7 +31,7 @@ class Dataset(object):
         return data, label
 
     def make_batch(self, batch_size):
-        records = self.get_tfrecords()
+        records = tf.data.Dataset.list_files(self.pattern)
         dataset = tf.data.TFRecordDataset(records).repeat()
         dataset = dataset.map(self.parser, num_parallel_calls=batch_size)
 
@@ -56,4 +52,41 @@ class Dataset(object):
         else:
             return 40
 
+class NpDataset(object):
+    def __init__(self, pattern, training=True):
+        self.pattern = pattern
+        self.training = training
 
+    def get_dataset(self):
+        paths = glob.glob(self.pattern)
+        all_data = np.empty((len(paths), 1, definitions.height, definitions.width))
+        all_labels = np.empty(len(paths))
+        for idx, path in enumerate(paths):
+            data = np.load(path)
+            data = np.reshape(data, (1, data.shape[0], data.shape[1]))
+            if 'frb' in path:
+                label = 0
+            elif 'psr' in path:
+                label = 1
+            else:
+                label = 2
+            all_data[idx, :, :, :] = data
+            all_labels[idx] = label
+        all_data = all_data.astype('float32')
+        all_labels = all_labels.astype('int64')
+        dataset = (all_data[:10], all_labels[:10])
+        dataset = tf.data.Dataset.from_tensor_slices(dataset)
+        return dataset
+
+    def make_batch(self, batch_size):
+        dataset = self.get_dataset().repeat()
+        dataset = dataset.batch(batch_size)
+
+        if self.training:
+            min_queue = int(10 * 0.4)
+            buffer_size = min_queue + (3 * batch_size)
+            dataset = dataset.shuffle(buffer_size=buffer_size)
+
+        iterator = dataset.make_one_shot_iterator()
+        data_batch, label_batch = iterator.get_next()
+        return data_batch, label_batch
