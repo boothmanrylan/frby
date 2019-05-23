1970493ecd37dbe1aa0f53f6d846e871af8453bf
Rylan Boothman <boothmanrylan@gmail.com>
2019-05-23 13:55:27 -0400

    Define model as estimator from start instead of converting after the fact

--- model.py
+++ model.py
@@ -20,18 +20,18 @@ tf.app.flags.DEFINE_integer('num_gpus', 4,
                             'Number of GPUs used for training and testing')
 tf.app.flags.DEFINE_integer('batch_size', 64,
                             'batch size, will be multiplied by NUM_GPUS')
-tf.app.flags.DEFINE_integer('train_steps', 100,
+tf.app.flags.DEFINE_integer('train_steps', 10000,
                             'Number of steps used during training')
-tf.app.flags.DEFINE_integer('test_steps', 10000,
+tf.app.flags.DEFINE_integer('eval_steps', 10000,
                             'Number of steps used during testing')
 tf.app.flags.DEFINE_string('train_pattern',
-                           '/scratch/r/rhlozek/rylan/tfrecords/train-00010*',
+                           '/scratch/r/rhlozek/rylan/tfrecords/train-*',
                            'Unix file pattern pointing to training records')
-tf.app.flags.DEFINE_string('test_pattern',
-                           '/scratch/r/rhlozek/rylan/tfrecords/val-0000*',
+tf.app.flags.DEFINE_string('eval_pattern',
+                           '/scratch/r/rhlozek/rylan/tfrecords/evaluate-*',
                            'Unix file pattern pointing to test records')
 tf.app.flags.DEFINE_string('validation_pattern',
-                           '/scratch/r/rhlozek/rylan/tfrecords/VAL*',
+                           '/scratch/r/rhlozek/rylan/tfrecords/validate-*',
                            'Unix file pattern pointing to validation records')
 tf.app.flags.DEFINE_string('checkpoint_path',
                            '/scratch/r/rhlozek/rylan/models/defualt',
@@ -40,47 +40,9 @@ tf.app.flags.DEFINE_string('base_model', 'resnet',
                            'Keras application to use as the base model')
 tf.app.flags.DEFINE_boolean('classification', True,
                             'Whether to classify samples or predict dm')
+tf.app.flags.DEFINE_integer("seed", 12345, "Seed for reproducibility")
 
 FLAGS = tf.app.flags.FLAGS
-
-
-class LogitModel(tf.keras.Model):
-    """
-    Wrapper for an already defined model, when training returns the softmax of
-    the output logits, but returns the logits themselves when testing to allow
-    for temperature scaling post training.
-    """
-    def __init__(self, model):
-        """
-        model should be an already defined tf.keras.Model whose final layer
-        returns logits not a class prediction.
-        """
-        super(LogitModel, self).__init__()
-        self.model = model
-
-    def call(self, inputs, training=False):
-        logits = self.model(inputs)
-        if training:
-            return tf.nn.softmax(logits)
-        else:
-            return logits
-
-def extract_labels(dataset):
-    dataset = data_fn()
-    iter = dataset.make_one_shot_iterator()
-    image, label = iter.get_next()
-
-    labels = []
-
-    with tf.Session() as sess:
-        try:
-            while True:
-                step_label = sess.run(label)
-                labels.append(step_label)
-        except tf.errors.OutOfRangeError:
-            pass
-
-    return labels
 
 
 def temp_scaling(logits, labels, sess, maxiter=50):
@@ -103,39 +65,62 @@ def temp_scaling(logits, labels, sess, maxiter=50):
     nll_loss = sess.run(nll_loss_op)
     temperature = sess.run(temp_var)
 
-    return temp_var
+    return temperature
 
 
-def inverse_softmax(x):
-    """
-    Given an array of outputs of the softmax function return an array of inputs
-    """
-    return np.log((-x * (np.sum(np.exp(x), axis=1, keepdims=True) - np.exp(x))) / (x - 1))
+def get_prediction_probabilities(validation_dataset, eval_dataset, params, temp=None):
+    val_images, val_labels = validation_dataset.make_one_shot_iterator().get_next()
+    images, labels = eval_dataset.make_one_shot_iterator().get_next()
 
+    val_predictions = model_fn(val_images, val_labels,
+                               tf.estimator.ModeKeys.EVAL, params).predictions
+    predictions = model_fn(images, labels,
+                           tf.estimator.ModeKeys.EVAL, params).predictions
 
-@autograph.convert(recursive=True)
-def predict(estimator, data, temperature):
-    iter = data.make_one_shot_iterator()
-    probabilities = []
-    predictions = []
-    accuracies = []
-    autograph.set_element_type(probabilities, tf.float32)
-    autograph.set_element_type(predictions, tf.int)
-    autograph.set_element_type(accuracies, tf.float32)
-    try:
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_path)
+        saver.restore(sess, ckpt.model_checkpoint_path)
+
+        if temp is None: # use temp_scaling to find temperature
+            val_logits = []
+            val_labels = []
+            while True:
+                try:
+                    preds, lbls = sess.run([val_predictions, val_labels])
+                    val_logits += preds['logits']
+                    val_labels += lbls
+                except tf.errors.OutOfRangeError:
+                    break
+
+            temp = temp_scaling(val_logits, val_labels, sess)
+
+        eval_logits = []
+        eval_labels = []
         while True:
-            x, y = iter.get_next()
-            y_p = estimator.predict(x)
-            scaled_y_p = tf.divide(y_p, temperature)
-            prob = tf.softmax(scaled_y_p)
-            pred = tf.argmax(prob, axis=1)
-            acc = tf.keras.metrics.categorical_accuracy(y, y_p)
-            probabilities.append(prob)
-            accuracies.append(acc)
-            predictions.append(pred)
-    except tf.errors.OutOfRangeError:
-        pass
-    return autograph.stack(probabilities), autograph.stack(predictions), tf.reduce_mean(autograph.stack(accuracies))
+            try:
+                preds, lbls = sess.run([predictions, labels])
+                eval_logits += preds['logits']
+                eval_labels += lbls
+            except tf.errors.OutOfRangeError:
+                break
+
+        logits = np.asarray(logits) / temp
+        probs = np.exp(logits) / np.sum(np.exp(logits), axis=1, keepdims=True)
+        predictions = np.argmax(probs, axis=1)
+
+        accuracy = tf.metrics.accuracy(labels, predictions)
+        confusion_matrix = tf.confusion_matrix(np.argmax(labels, axis=1),
+                                               np.argmax(predictions, axis=1))
+
+        output = {
+            'logits': logits,
+            'probs': probs,
+            'class_preds': predictions,
+            'accuracy': accuracy,
+            'confusion_matrix': confusion_matrix
+        }
+        return predictions, probs, eval_labels
 
 
 def parse_fn(example):
@@ -154,6 +139,7 @@ def parse_fn(example):
 
     parsed = tf.parse_single_example(example, example_fmt)
     image = tf.image.decode_image(parsed['image'], channels=1)
+    image = tf.cast(image, tf.float32)
     image.set_shape(SHAPE)
 
     if FLAGS.classification:
@@ -192,58 +178,65 @@ def get_base_model(model_name):
 
 
 def model_fn(features, labels, mode, params):
-    x = tf.feature_column.input_layer(features, params["feature_columns"])
-    x = tf.layers.conv2D(x, 64, (3, 3), activation='relu', padding='same', name='block1_conv1')
-    x = tf.layers.conv2D(x, 64, (3, 3), activation='relu', padding='same', name='block1_conv2')
-    x = tf.layers.MaxPooling2D(x, (2, 2), strides=(2, 2) name='block1_pool')
-    x = tf.layers.conv2D(x, 128 (3, 3), activation='relu', padding='same', name='block2_conv1')
-    x = tf.layers.conv2D(x, 128 (3, 3), activation='relu', padding='same', name='block2_conv2')
-    x = tf.layers.MaxPooling2D(x, (2, 2), strides=(2, 2), name='block2_pool')
-    x = tf.layers.conv2D(x, 256 (3, 3), activation='relu', padding='same', name='block3_conv1')
-    x = tf.layers.conv2D(x, 256 (3, 3), activation='relu', padding='same', name='block3_conv2')
-    x = tf.layers.conv2D(x, 256 (3, 3), activation='relu', padding='same', name='block3_conv3')
-    x = tf.layers.conv2D(x, 256 (3, 3), activation='relu', padding='same', name='block3_conv4')
-    x = tf.layers.MaxPooling2D(x, (2, 2), strides=(2, 2), name='block3_pool')
-    x = tf.layers.conv2D(x, 512, (3, 3), activation='relu', padding='same', name='block4_conv1')
-    x = tf.layers.conv2D(x, 512, (3, 3), activation='relu', padding='same', name='block4_conv2')
-    x = tf.layers.conv2D(x, 512, (3, 3), activation='relu', padding='same', name='block4_conv3')
-    x = tf.layers.conv2D(x, 512, (3, 3), activation='relu', padding='same', name='block4_conv4')
-    x = tf.layers.MaxPooling2D(x, (2, 2), strides=(2, 2), name='block4_pool')
-
-
-
-def build_model():
-    if FLAGS.classification:
-        output_neurons = CLASSES
-        activation = tf.nn.softmax
-        loss = 'categorical_crossentropy'
-        metrics = ['accuracy']
-    else:
-        output_neurons = 1
-        activation = None
-        loss = 'mean_squared_error'
-        metrics = ['mean_squared_error', 'mean_absolute_error']
-
-    base = get_base_model(FLAGS.base_model)
-    model = tf.keras.model.Sequential([
-        base(include_top=False, weights=None, input_shape=SHAPE, pooling='max'),
-        tf.keras.layers.Dense(output_neurons, activation=activation)
+    base_model = get_base_model(params["model_name"])
+    model = tf.keras.Sequential([ # this might need to be converted to the functional api
+        base_model(include_top=False, input_shape=SHAPE, weights=None),
+        Flatten(name='flatten'),
+        Dense(4096, activation='relu', name='dense1'),
+        Dense(4096, activation='relu', name='dense2'),
+        Dense(params['n_classes'], activation=None, name='logits')
     ])
 
-    mode.compile(loss=loss, optimizer=tf.train.AdamOptimizer(), metrics=metrics)
-
-    return model
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        logits = model(features, training=True)
+        optimizer = tf.train.AdamOptimizer()
+        if params['n_classes'] > 1:
+            loss = tf.losses.softmax_cross_entropy(labels, logits)
+        else:
+            loss = tf.losses.mean_squared_error(labels, logits)
+        train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+    elif mode == tf.estimator.ModeKeys.PREDICT:
+        logits = model(features, training=False)
+        if params['n_classes'] > 1:
+            predicted_classes = tf.argmax(logits, 1)
+            predictions = {
+                'class_ids': predicted_classes[:, tf.newaxis],
+                'logits': logits
+            }
+        else:
+            predictions = logits
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+    elif mode == tf.estimator.ModeKeys.EVAL:
+        logits = model(features, training=False)
+        if params['n_classes'] > 1:
+            loss = tf.losses.softmax_cross_entropy(labels, logits)
+        else:
+            loss = tf.losses.mean_squared_error(labels, logits)
+        if params['n_classes'] > 1:
+            predictions = tf.argmax(logits, axis=1)
+            decoded_labels = tf.argmax(labels, axis=1)
+            accuracy = tf.metrics.accuracy(labels=decoded_labels,
+                                           predictions=predictions,
+                                           name='acc_op')
+            # eval_metric_ops need to return an update_op
+            # confusion_matrix = tf.confusion_matrix(decoded_labels,
+            #                                        predictions)
+            metrics = {'accuracy': accuracy}
+                       #'confusion_matrix': confusion_matrix}
+            tf.summary.scalar('accuracy', accuracy[1])
+        else:
+            mse = tf.keras.metrics.mean_squared_error(labels, logits)
+            mae = tf.keras.metrics.mean_absolute_error(labels, logits)
+            metrics = {'mean_squared_error': mse,
+                       'mean_absolute_error': mae}
+        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
 
 
 def main(argv=None):
-    model = build_model()
-
     # log model parameters:
     for flag in FLAGS.flag_values_dict():
         print("{}:\t{}".format(flag, FLAGS[flag].value))
-
-    # log model overview
-    model.summary()
 
     devices = ["/gpu:{}".format(x) for x in range(FLAGS.num_gpus)]
     mirror = tf.distribute.MirroredStrategy(devices)
@@ -253,59 +246,27 @@ def main(argv=None):
                                     model_dir=FLAGS.checkpoint_path,
                                     tf_random_seed=FLAGS.seed)
 
-    estimator = tf.keras.estimator.model_to_estimator(model, config=config)
+    params={'n_classes': CLASSES if FLAGS.classification else 1,
+            'model_name': FLAGS.base_model}
+    estimator = tf.estimator.Estimator(
+        model_fn=model_fn,
+        params=params,
+        config=config
+    )
 
     train_input = functools.partial(input_fn, pattern=FLAGS.train_pattern, repeat=True)
     estimator.train(train_input, steps=FLAGS.train_steps)
 
-    ##########################################################################
-    ##########################################################################
-    val_dataset = input_fn(FLAGS.validation_pattern, repeat=False)
-    val_images, val_labels = val_dataset.make_one_shot_iterator().get_next()
+    # val_dataset = input_fn(FLAGS.validation_pattern, repeat=False)
+    # eval_dataset = input_fn(FLAGS.eval_pattern, repeat=False)
 
-    val_predictions = estimator.predict(val_images, val_labels)
+    # results = get_prediction_probabilities(val_dataset, eval_dataset, params)
+    # for key, value in results.items():
+    #     print(key, value)
 
-    dataset = input_fn(FLAGS.test_pattern, repeat=False)
-    images, labels = dataset.make_one_shot_iterator().get_next()
-
-    predictions = estimator.predict(images, labels)
-
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
-        ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_path)
-        saver.restore(sess, ckpt.model_checkpoint_path)
-
-        val_prediction_values = []
-        val_label_values = []
-        while True:
-            try:
-                preds, lbls = sess.run([val_predictions, val_labels])
-                prediction_values += preds
-                label_values += lbls
-            except tf.errors.OutOfRangeError:
-                break
-
-        temp = temp_scaling(val_prediction_values, val_label_values, sess)
-
-        prediction_values = []
-        label_values = []
-        while True:
-            try:
-                preds, lbls = sess.run([predictions, labels])
-                prediction_values += preds
-                label_values += lbls
-            except tf.errors.OutOfRangeError:
-                break
-    ##########################################################################
-    ##########################################################################
-
-    # eval_data = functools.partial(input_fn, pattern=FLAGS.test_pattern, repeat=False)
-    # probs, preds, acc = evaluate(estimator, eval_data, temperature)
-
-    # print(probs)
-    # print(preds)
-    # print(acc)
-
+    eval_input = functools.partial(input_fn, pattern=FLAGS.eval_pattern, repeat=False)
+    results = estimator.evaluate(eval_input, steps=FLAGS.eval_steps)
+    print(results)
 
 if __name__ == '__main__':
     tf.app.run()
